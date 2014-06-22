b12e1309869b0ea7ea9e821f12b7f2e556303aa6
Luboslav Ivanov <killer18ys@gmail.com>
2014-06-22 15:50:49 +0300

    Change

---
+++ scripts/javascript.js
@@ -0,0 +1,53 @@
+(function(){
+	var numberOfQuestions = document.getElementsByClassName("input-test-number")[0],
+		generateBtn = document.getElementsByClassName("btn-generate")[0]
+		hardQuestions = document.getElementById("hardQuestions"),
+		testField = document.getElementById("x-handlebar-template");
+
+
+
+
+
+
+	function randomHexGen(difficulty){
+		if (difficulty == "easy") {
+			var letters = '0F'.split(''),
+				hex = '0x';
+			for (var i = 0; i < 6; i++) {
+				hex += letters[Math.floor(Math.random() * 2)];
+			}
+			return hex;
+		}
+		else if(difficulty == "hard"){
+			var letters = '0123456789abcdef'.split(''),
+				hex = '0x';
+			for (var i = 0; i < 6; i++) {
+				hex += letters[Math.floor(Math.random() * 16)];
+			}
+			return hex;
+		}
+	}
+
+
+
+	
+//Handlebar Template
+
+var source = document.getElementById("entry-template").innerHTML,
+	template = Handlebars.compile(source),
+
+	context = [{
+		name: "Gosho",
+		date: "2014"
+	},{
+		name: "Pesho",
+		date: "2015"
+	}];
+
+	html = template({
+		context:context
+	})
+
+	testField.innerHTML += html;
+
+}());
